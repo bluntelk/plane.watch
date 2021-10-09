87ea0667548ad6a5801220770bd15b889def0133
Jason Playne <jason@jasonplayne.com>
2021-10-09 16:33:20 +0800

    first cut at breaking the world up into tiles and tagging locations with it

--- dev-scripts/firehose.sh
+++ dev-scripts/firehose.sh
@@ -2,4 +2,5 @@ bin/plane.watch.client \
   --fetch=beast://crawled.mapwithlove.com:3004 \
   --sink=amqp://guest:guest@localhost:5672/pw \
   --rabbit-queue=location-updates \
+  --debug \
   daemon
